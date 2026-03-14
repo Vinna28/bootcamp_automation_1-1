d6c04ab5a7307c31e110d3f45d50448ecc9b6b14
Fadhli Maulidri <fadhlimaulidri@gmail.com>
2026-03-14 12:08:26 +0700

    forgot password negative (#3)

--- tests/forgot-password.spec.ts
+++ tests/forgot-password.spec.ts
@@ -1,6 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('test case positive', async ({ page }) => {
+  await page.goto('https://www.emra.com/login');
+  await page.getByRole('link', { name: 'Forgot?' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).fill('testingemrachat@yopmail.com');
+  await page.getByRole('button', { name: 'Reset Password' }).click();
+});
+
+test('test case negative', async ({ page }) => {
   await page.goto('https://www.emra.com/login');
   await page.getByRole('link', { name: 'Forgot?' }).click();
   await page.getByRole('textbox', { name: 'Email' }).click();
