a23cd1f49d3889ecf9fd282186bf7f329887bdda
Fadhli Maulidri <fadhlimaulidri@gmail.com>
2026-03-14 11:44:58 +0700

    forgot password

---
+++ tests/forgot-password.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.emra.chat/login');
+  await page.getByRole('link', { name: 'Forgot?' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).fill('testingemrachat@yopmail.com');
+  await page.getByRole('button', { name: 'Reset Password' }).click();
+});
